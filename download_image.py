57514adacb1b6fe610d94c399c4cf1ffadcc02a2
ZHartem21 <zhartem21@gmail.com>
2022-11-10 11:54:25 +0300

    Update makedirs

--- download_image.py
+++ download_image.py
@@ -3,14 +3,9 @@ import os
 import requests
 
 
-def create_directory(directory_name):
-    if not os.path.exists(directory_name):
-        os.makedirs(directory_name)
-
-
 def download_image(url, directory, filename, params={}):
     response = requests.get(url, params=params)
     response.raise_for_status()
-    create_directory(directory)
+    os.makedirs(directory, exist_ok=True)
     with open(os.path.join(directory, filename), 'wb') as file:
         file.write(response.content)
